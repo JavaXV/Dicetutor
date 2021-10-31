651bdbc1fa6cc096683efcce3c4c878b85144eb5
Christian <88792710+JavaXV@users.noreply.github.com>
2021-10-30 21:48:22 -0700

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<!-- index139:25 GMT -->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="keywords" content="">
+<title>DiceTutors</title>
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
+<link rel="apple-touch-icon" href="images/apple-touch-icon.png"/>
+<link rel="apple-touch-icon" sizes="57x57" href="images/apple-touch-icon-57x57.png"/>
+<link rel="apple-touch-icon" sizes="72x72" href="images/xapple-touch-icon-72x72.png.pagespeed.ic.lf5d8kCpOf.png"/>
+<link rel="apple-touch-icon" sizes="76x76" href="images/xapple-touch-icon-76x76.png.pagespeed.ic.ATZZpSeito.png"/>
+<link rel="apple-touch-icon" sizes="114x114" href="images/xapple-touch-icon-114x114.png.pagespeed.ic.Fi5O5s2tzL.png"/>
+<link rel="apple-touch-icon" sizes="120x120" href="images/xapple-touch-icon-120x120.png.pagespeed.ic.uPQH0sygdV.png"/>
+<link rel="apple-touch-icon" sizes="144x144" href="images/xapple-touch-icon-144x144.png.pagespeed.ic.yZ9-_sm5OF.png"/>
+<link rel="apple-touch-icon" sizes="152x152" href="images/xapple-touch-icon-152x152.png.pagespeed.ic.gThaVrKtXF.png"/>
+<link rel="apple-touch-icon" sizes="180x180" href="images/xapple-touch-icon-180x180.png.pagespeed.ic.Q8Pmsj5fQM.png"/>
+<link rel="stylesheet" type="text/css" href="rs-plugin/css/A.settings.css.pagespeed.cf.xeOyGChsgq.css" media="screen"/>
+<link rel="stylesheet" type="text/css" href="A.fonts%2c%2c_font-awesome-4.3.0%2c%2c_css%2c%2c_font-awesome.min.css%2bcss%2c%2c_bootstrap.css%2bcss%2c%2c_animate.css%2cMcc.kSNwpaaMDX.css.pagespeed.cf.w2G3xGgFf0.css"/>
+<link rel="stylesheet" type="text/css" href="css/A.menu.css.pagespeed.cf.0_hLwXzYkZ.css">
+<link rel="stylesheet" type="text/css" href="css/carousel.css%2bbbpress.css.pagespeed.cc.aXsJ7_Y__m.css"/>
+<link rel="stylesheet" type="text/css" href="A.style.css%2bcss%2c%2c_custom.css%2cMcc.HvWh1qoob-.css.pagespeed.cf.pWH5huNcWh.css"/>
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body style="height:400px;">
+<div id="loader">
+<div class="loader-container">
+<img src="images/site.gif" alt="" class="loader-site">
+</div>
+</div>
+<div id="wrapper">
+<div class="row-offcanvas row-offcanvas-left">
+<div id="sidebar-fix" class="sidebar-offcanvas">
+<div class="side-logo clearfix">
+<a href="index-2.html"><img src="images/Dice Tutuors.png" height="100: width="90" alt=""></a>
+</div>
+<ul class="sidebar-nav clearfix">
+<li class="active"><a href="/teacherprofile">Join Us</a></li>
+<li class="active"><a href="/login">Admin Login</a></li>
+<li class="active"><a href="/makepayment">Make Payment</a></li>
+
+</ul>
+<br><br><br><br>
+<div class="side-copyright">
+<ul class="list-inline">
+<li>
+<a class="social" href="#"><i class="fa fa-facebook"></i></a>
+<a class="social" href="#"><i class="fa fa-twitter"></i></a>
+<a class="social" href="#"><i class="fa fa-google-plus"></i></a>
+<a class="social" href="#"><i class="fa fa-linkedin"></i></a>
+</li>
+</ul>
+<p>© 2021 Devosta Solution.<br> by <a href="#">www.devosta.ng</a></p>
+</div>
+</div>
+<div id="main">
+<div class="visible-md visible-xs visible-sm mobile-menu">
+<button type="button" class="btn btn-default btn-sm" data-toggle="offcanvas"><i class="fa fa-bars"></i></button>
+</div>
+<section class="slider-section">
+<div class="tp-banner-container">
+<div class="tp-banner">
+<ul>
+<li data-transition="fade" data-slotamount="1" data-masterspeed="500" data-thumb="upload/dice2.png" data-saveperformance="off" data-title="Slide">
+<img src="upload/dices.png" alt="fullslide1" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+<!--<div class="tp-caption slider_layer_05 text-center lft tp-resizeme" data-x="center" data-y="240" data-speed="1000" data-start="600" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="1000" style="z-index: 9; max-width: auto; max-height: auto; white-space: nowrap;"><i class="fa fa-graduation-cap"></i>
+</div>-->
+<!--<div class="tp-caption slider_layer_01 text-center lft tp-resizeme" data-x="center" data-y="350" data-speed="1000" data-start="600" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="1000" style="z-index: 9; max-width: auto; max-height: auto; font-size:40px; position:absolute; white-space: nowrap;">Devosta Solution-->
+</div>
+
+</li>
+</ul>
+</div>
+</div>
+</section>
+
+
+<script src="js/jquery.min.js.pagespeed.jm.iDyG3vc4gw.js"></script>
+<script src="js/bootstrap.min.js%2bretina.js%2bwow.js.pagespeed.jc.pMrMbVAe_E.js"></script><script>eval(mod_pagespeed_gFRwwUbyVc);</script>
+<script>eval(mod_pagespeed_rQwXk4AOUN);</script>
+<script>eval(mod_pagespeed_U0OPgGhapl);</script>
+<script src="js/carousel.js%2bparallax.min.js%2bcustom.js.pagespeed.jc.lVSvRd9-tY.js"></script><script>eval(mod_pagespeed_6Ja02QZq$f);</script>
+<script>eval(mod_pagespeed_AZ_gON44eP);</script>
+<script>eval(mod_pagespeed_KxQMf5X6rF);</script>
+<script src="rs-plugin/js/jquery.themepunch.tools.min.js.pagespeed.jm.0PLSBOOLZa.js"></script>
+<script src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<script>jQuery(document).ready(function(){jQuery('.tp-banner').show().revolution({dottedOverlay:"none",delay:16000,startwidth:1170,startheight:820,hideThumbs:200,thumbWidth:100,thumbHeight:50,thumbAmount:5,navigationType:"none",navigationArrows:"solo",navigationStyle:"preview3",touchenabled:"on",onHoverStop:"on",swipe_velocity:0.7,swipe_min_touches:1,swipe_max_touches:1,drag_block_vertical:false,parallax:"mouse",parallaxBgFreeze:"on",parallaxLevels:[10,7,4,3,2,5,4,3,2,1],parallaxDisableOnMobile:"off",keyboardNavigation:"off",navigationHAlign:"center",navigationVAlign:"bottom",navigationHOffset:0,navigationVOffset:20,soloArrowLeftHalign:"left",soloArrowLeftValign:"center",soloArrowLeftHOffset:20,soloArrowLeftVOffset:0,soloArrowRightHalign:"right",soloArrowRightValign:"center",soloArrowRightHOffset:20,soloArrowRightVOffset:0,shadow:0,fullWidth:"on",fullScreen:"off",spinner:"spinner4",stopLoop:"off",stopAfterLoops:-1,stopAtSlide:-1,shuffle:"off",autoHeight:"off",forceFullWidth:"off",hideThumbsOnMobile:"off",hideNavDelayOnMobile:1500,hideBulletsOnMobile:"off",hideArrowsOnMobile:"off",hideThumbsUnderResolution:0,hideSliderAtLimit:0,hideCaptionAtLimit:0,hideAllCaptionAtLilmit:0,startWithSlide:0,fullScreenOffsetContainer:""});});</script>
+<script type="text/javascript">(function($){"use strict";$('[data-toggle=offcanvas]').click(function(){$('.row-offcanvas').toggleClass('active');});})(jQuery);</script>
+</body>
+
+<!-- index139:29 GMT -->
+</html>
